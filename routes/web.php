b7e0f5fd5f593d3a0666c7d75caef1ddd0421390
hiwkaochiphaii <anuwat.suri@vru.ac.th>
2026-07-11 03:23:51 +0700

    week03

--- routes/web.php
+++ routes/web.php
@@ -44,18 +44,27 @@ Route::get("/gallery/cat", function () {
     $cat = "https://www.sideshow.com/cdn-cgi/image/height=850,quality=90,f=auto/https://www.sideshow.com/storage/product-images/910233/black-panther-deluxe_marvel_gallery_61eb5a329c25b.jpg";
     return view("test/cat", compact("cat"));
 });
+Route::get('/active/index', function () {
+    return view('active/index');
+})->name('index');
 
-Route::get("/teacher" , function (){
-	return view("teacher");
-});
-
-Route::get("/student" , function (){
-	return view("student");
-});
-
-Route::get("/theme" , function (){
-	return view("theme");
-});
-
+Route::get('/active/about', function () {
+    return view('active/about');
+})->name('about');
+Route::get('/active/services', function () {
+    return view('active/services');
+})->name('services');
+Route::get('/active/portfolio', function () {
+    return view('active/portfolio');
+})->name('portfolio');
+Route::get('/active/team', function () {
+    return view('active/team');
+})->name('team');
+Route::get('/active/blog', function () {
+    return view('active/blog');
+})->name('blog');
+Route::get('/active/contact', function () {
+    return view('active/contact');
+})->name('contact');
 
 require __DIR__.'/auth.php';
